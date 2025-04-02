7a7fec87564f3ff02e2a4b3c097d77dff786d482
Axel Ibarrola <axelibarro@gmail.com>
2025-04-02 11:16:50 -0300

    creada la variable de puerto e inicializado el server

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 const express = require('express');
 const app = express();
+require('dotenv').config()
+const PORT = process.env.PORT() || 3000;
+
+app.listen(PORT,
+    () => console.log(`Servidor corriento en http:localhost:${PORT}`)
+)
 
