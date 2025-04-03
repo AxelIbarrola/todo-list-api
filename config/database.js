2a8d35aef90dfd58c9cb6260e716841e51341f62
Axel Ibarrola <axelibarro@gmail.com>
2025-04-03 15:14:47 -0300

    agregada la conexión a la base de datos

---
+++ config/database.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT,
+        port: process.env.DB_PORT,
+        logging: false
+    }
+)
+
+
+sequelize.authenticate()
+    .then(() => console.log('Conexión exitosa con PostgreSQL'))
+    .catch(err => console.error(`Error de conexión: ${err}`))
+
+module.exports = sequelize;
